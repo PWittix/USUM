12976651024f0e31848de7e5df4d804f8bc6d5e3
PWittix <wittix@outlook.com>
2017-12-08 21:49:12 -0800

    stuff
    Bulk upload

---
+++ download.js
@@ -0,0 +1,21 @@
+//vars
+
+var lycanrocDusk = document.getElementById("dlLycanroc-Dusk");
+var stakataka = document.getElementById("dlStakataka");
+var blacephalon = document.getElementById("dlBlacephalon");
+var necrozmaUltra = document.getElementById("dlNecrozma-Ultra");
+var naganadel  = document.getElementById("dlNaganadel");
+/*
+var  = document.getElementById("dl");
+var  = document.getElementById("dl");
+var  = document.getElementById("dl");
+var  = document.getElementById("dl");
+*/
+lycanrocDusk.addEventListener("click");
+
+
+
+function dlLD(){
+	"use strict";
+	
+}
